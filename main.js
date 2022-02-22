496fbadab8977a391a93c166952c6d5d06eb17e2
Daniel Lafontant <ldanieljajr@gmail.com>
2022-02-22 14:58:48 -0500

    competed

--- main.js
+++ main.js
@@ -32,21 +32,175 @@
 
 // })
 
-//Part3
+// //Part3
+// var btn = document.getElementById("btn")
+// var animalContainer = document.getElementById("animal-info")
+
+// btn.addEventListener("click", function () {
+//     var ourRequest = new XMLHttpRequest();
+//     ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json')
+//     ourRequest.onload = function () {
+//         var ourData = JSON.parse(ourRequest.responseText);
+//         renderHTML(ourData)
+//     };
+//     ourRequest.send();
+// })
+// // to add html json data on the page
+// function renderHTML(data){
+// // (method) Element.insertAdjacentHTML(position: InsertPosition, text: string): void
+//  animalContainer.insertAdjacentHTML('beforeend','testin ABC' )
+// }
+
+// //Part4
+// var btn = document.getElementById("btn")
+// var animalContainer = document.getElementById("animal-info")
+
+// btn.addEventListener("click", function () {
+//     var ourRequest = new XMLHttpRequest();
+//     ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json')
+//     ourRequest.onload = function () {
+//         var ourData = JSON.parse(ourRequest.responseText);
+//         renderHTML(ourData)
+//     };
+//     ourRequest.send();
+// })
+// function renderHTML(data){
+// // var htmlString = " this is mr"
+// //animalContainer.insertAdjacentHTML('beforeend', htmlString )
+// var htmlString = ""
+// for(i = 0; i < data.length; i++){
+//     htmlString += "<h1>" + data[i].name + " is a " + data[i].species + ".</h1>";
+// }
+//  animalContainer.insertAdjacentHTML('beforeend', htmlString )
+// }
+
+
+// //Part 5
+// //to add more urls data
+// var pageCounter = 1;
+// var btn = document.getElementById("btn")
+// var animalContainer = document.getElementById("animal-info")
+
+// btn.addEventListener("click", function () {
+//     var ourRequest = new XMLHttpRequest();
+//     //turn url dynamic
+//     ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json')
+//     ourRequest.onload = function () {
+//         var ourData = JSON.parse(ourRequest.responseText);
+//         renderHTML(ourData)
+//     };
+//     // increament oage counter value for data
+//     ourRequest.send();
+//     pageCounter++;
+// })
+// function renderHTML(data){
+// // var htmlString = " this is mr"
+// //animalContainer.insertAdjacentHTML('beforeend', htmlString )
+// var htmlString = ""
+// for(i = 0; i < data.length; i++){
+//     htmlString += "<h1>" + data[i].name + " is a " + data[i].species + ".</h1>";
+// }
+//  animalContainer.insertAdjacentHTML('beforeend', htmlString )
+// }
+
+
+// //Part 5
+// //to remove button after couples click
+// var pageCounter = 1;
+// var btn = document.getElementById("btn")
+// var animalContainer = document.getElementById("animal-info")
+
+// btn.addEventListener("click", function () {
+//     var ourRequest = new XMLHttpRequest();
+//     ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json')
+//     ourRequest.onload = function () {
+//         var ourData = JSON.parse(ourRequest.responseText);
+//         renderHTML(ourData)
+//     };
+//     ourRequest.send();
+//     pageCounter++;
+//     if (pageCounter > 3){
+//         btn.classList.add("hide-me");
+//     }
+// })
+// function renderHTML(data){
+// var htmlString = ""
+// for(i = 0; i < data.length; i++){
+//     htmlString += "<h1>" + data[i].name + " is a " + data[i].species + ".</h1>";
+// }
+//  animalContainer.insertAdjacentHTML('beforeend', htmlString )
+// }
+
+
+
+// //Part 6
+// //add aditional data from the JSON
+// var pageCounter = 1;
+// var btn = document.getElementById("btn")
+// var animalContainer = document.getElementById("animal-info")
+
+// btn.addEventListener("click", function () {
+//     var ourRequest = new XMLHttpRequest();
+//     ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json')
+//     ourRequest.onload = function () {
+//         var ourData = JSON.parse(ourRequest.responseText);
+//         renderHTML(ourData)
+//     };
+//     ourRequest.send();
+//     pageCounter++;
+//     if (pageCounter > 3){
+//         btn.classList.add("hide-me");
+//     }
+// })
+// function renderHTML(data){
+// var htmlString = ""
+// for(i = 0; i < data.length; i++){
+//     htmlString += "<h1>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
+//     for (ii = 0; ii < data[i].foods.likes.length; ii++){
+//         // now concates the resultes of the food array with htmlString
+//         htmlString += data[i].foods.likes[ii];
+//     }
+//      htmlString += ".</h1>";
+// }
+//  animalContainer.insertAdjacentHTML('beforeend', htmlString )
+// }
+
+
+
+
+
+//Part 6
+//add aditional  and spacedata from the JSON
+var pageCounter = 1;
 var btn = document.getElementById("btn")
 var animalContainer = document.getElementById("animal-info")
 
 btn.addEventListener("click", function () {
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json')
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json')
     ourRequest.onload = function () {
         var ourData = JSON.parse(ourRequest.responseText);
         renderHTML(ourData)
     };
     ourRequest.send();
+    pageCounter++;
+    if (pageCounter > 3){
+        btn.classList.add("hide-me");
+    }
 })
-// to add html json data on the page
 function renderHTML(data){
-// (method) Element.insertAdjacentHTML(position: InsertPosition, text: string): void
- animalContainer.insertAdjacentHTML('beforeend','testin ABC' )
+var htmlString = ""
+for(i = 0; i < data.length; i++){
+    htmlString += "<h1>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
+    for (ii = 0; ii < data[i].foods.likes.length; ii++){
+        if (ii == 0){
+            htmlString += data[i].foods.likes[ii];
+        }else {
+            htmlString += " and " + data[i].foods.likes[ii];
+        }
+    }
+     htmlString += ".</h1>";
 }
+ animalContainer.insertAdjacentHTML('beforeend', htmlString )
+}
+
